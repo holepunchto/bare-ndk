d655becd845d830697d6c819158b743b1d23c2cc
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-05-29 14:08:10 +0200

    Add Lunte, publish workflow, and use `bare-build` v1 (#2)

--- runtime.js
+++ runtime.js
@@ -1,3 +1,7 @@
+const { type } = require('bare-build/constants')
+
+const { SHARED_LIBRARY } = type
+
 require.asset = require('require-asset')
 
 const prebuilds = {}
@@ -5,17 +9,45 @@ const prebuilds = {}
 exports.prebuilds = prebuilds
 
 prebuilds['android-x64'] = () => {
-  return require.asset('./prebuilds/android-x64/libbare.so', __filename)
+  return {
+    type: SHARED_LIBRARY,
+    path: require.asset('./prebuilds/android-x64/libbare.so', __filename),
+    dependencies: [
+      require.asset('./prebuilds/android-x64/bare/classes.dex', __filename),
+      require.asset('./prebuilds/android-x64/bare/libc++_shared.so', __filename)
+    ]
+  }
 }
 
 prebuilds['android-ia32'] = () => {
-  return require.asset('./prebuilds/android-ia32/libbare.so', __filename)
+  return {
+    type: SHARED_LIBRARY,
+    path: require.asset('./prebuilds/android-ia32/libbare.so', __filename),
+    dependencies: [
+      require.asset('./prebuilds/android-ia32/bare/classes.dex', __filename),
+      require.asset('./prebuilds/android-ia32/bare/libc++_shared.so', __filename)
+    ]
+  }
 }
 
 prebuilds['android-arm64'] = () => {
-  return require.asset('./prebuilds/android-arm64/libbare.so', __filename)
+  return {
+    type: SHARED_LIBRARY,
+    path: require.asset('./prebuilds/android-arm64/libbare.so', __filename),
+    dependencies: [
+      require.asset('./prebuilds/android-arm64/bare/classes.dex', __filename),
+      require.asset('./prebuilds/android-arm64/bare/libc++_shared.so', __filename)
+    ]
+  }
 }
 
 prebuilds['android-arm'] = () => {
-  return require.asset('./prebuilds/android-arm/libbare.so', __filename)
+  return {
+    type: SHARED_LIBRARY,
+    path: require.asset('./prebuilds/android-arm/libbare.so', __filename),
+    dependencies: [
+      require.asset('./prebuilds/android-arm/bare/classes.dex', __filename),
+      require.asset('./prebuilds/android-arm/bare/libc++_shared.so', __filename)
+    ]
+  }
 }
